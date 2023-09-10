0e3ee2d3d9b07e81c3bf9fd532f8ba7d52751846
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-09-10 15:01:11 -0300

    'Code Snippets'/ SQLQuery1.sql SQLQuery2.sql Settings/ Templates/

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE db_Biblioteca
+ON PRIMARY(
+NAME= db_Biblioteca,
+FILENAME='C:\Program Files\Microsoft SQL Server.MDF',
+SIZE=6MB,
+MAXSIZE=15MB,
+FILEGROWTH=10%
+)
+/* 'C:\Program Files\Microsoft SQL Server.MDF' */
